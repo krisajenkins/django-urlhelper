2c15da3335b80d46e798c5681307c2c755ea433a
Kris Jenkins <krisajenkins@gmail.com>
2011-10-04 09:45:43 +0100

    Adding the initial code, ported from another Django project of mine.

---
+++ urlhelper/management/commands/resolveurl.py
@@ -0,0 +1,17 @@
+from django.core.management.base		import BaseCommand
+from django.core.urlresolvers			import resolve, Resolver404
+
+class Command( BaseCommand ):
+	args = '<url url ...>'
+	help = 'URL-name lookup function. The Django-resolved name (plus some other details) will be printed for each URL supplied.'
+
+	def handle( self, *args, **options ):
+		for url in args:
+			try:
+				resolved = resolve( url )
+				print( "%s => %s" % ( url, resolved.url_name ) )
+				print( "\t%s" % resolved )
+			except Resolver404:
+				print( "%s => Not Found" %  url )
+
+			print
